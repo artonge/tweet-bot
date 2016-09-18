91972579462750fe0f716e3c1b0433b51a7dcd83
Louis <artonge.c@gmail.com>
2016-09-18 11:11:00 +0200

    Delete credentials.ts

--- src/credentials.ts
+++
@@ -1,6 +0,0 @@
-export const credentials = {
-  consumer_key        : "0aWjSbb2J0iWI01VXrHOWgg4d",
-  consumer_secret     : "GudhW1diZmcrCqou9iFER5wnoTRLcS8tIBH3vdE4rY3Diuc6O2",
-  access_token_key    : "3421948445-kwRr7ccpMo50WKhO1vgTIb10EZFpbqt5xu9Jdyk",
-  access_token_secret : "nh1QQ4KHNgoYbRl0ysnrAOP7ZTAHJuGXyKb6ZfZfVBVC7"
-};
