9e86c26b08be6dff76663e382a0dadac9c93b684
artonge <artonge.C@gmail.com>
2016-09-23 19:41:44 +0200

    use Twit instead of previous twitter npm lib Update functions to use Twit typescript interfaces Fix mistake in followers/subscribers comparaison

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,35 @@
-const Twitter = require('twitter');
+import * as Twit from 'twit';
 
 import { credentials } from './credentials';
 import { log, success, error } from './log';
 
 
-const SEARCHS: string = [
+const SEARCHS: [string] = [
   "retweet a gagner",
   "retweet to win",
   "RT to win",
   "RT concours"
-].join(',');
+];
 
 const FOLLOW_HISTORY: Array<number> = [];
 
 const MIN_15 = 15 * 60 * 1000; // 15 min
 
-const t_client = new Twitter(credentials);
+const T: Twit = new Twit(credentials);
 
 // A new unfollow batch every 15 min
 setInterval(function() { unfollowBatch("tulipe_fragile") }, MIN_15);
 
 
-var stream = t_client.stream('statuses/filter', {track: SEARCHS});
-stream.on('data', engage);
-stream.on('error', error);
+var stream = T.stream('statuses/filter', <Twit.Params>{ track: SEARCHS });
+stream.on('connected', ()=>success("connected"));
+stream.on('reconnect', ()=>success("reconnect"));
+stream.on('tweet', engage);
+stream.on('disconnect', ()=>success("disconnect"));
+stream.on('limit', (limit_object: any)=>success("disconnect" + limit_object.toString()));
+stream.on('warning', (warning_object: any)=>success("disconnect" + warning_object.toString()));
+stream.on('blocked', (blocked_object: any)=>success("disconnect" + blocked_object.toString()));
 
-
-function isTweet(object: any): boolean {
-	return typeof object.contributors == "object"
-			&& typeof object.id_str       == "string"
-			&& typeof object.text         == "string"
-}
 
 function canFollow(): boolean {
 	if (FOLLOW_HISTORY.length > 15) { // If 15 requests have been made
@@ -43,55 +42,54 @@ function canFollow(): boolean {
 	return true;
 }
 
-function follow(user: any) {
+function follow(user: Twit.Twitter.User) {
 	if (user.following) { error("Allready following");   return; }
 	if (!canFollow())   { /*error("Follow limit reached");*/ return; }
 
-	t_client.post('friendships/create', {user_id: user.id_str},  function(e: any, u: any, raw: any) {
+	T.post('friendships/create', <Twit.Params>{ user_id: user.id_str },  function(e: any, u: any, raw: any) {
 		if (e) error(e);
 		else FOLLOW_HISTORY.push((new Date()).getTime()); // Add the request in history
 	});
 }
 
-function unfollow(user: any) {
-	const time_diff: number = (new Date()).getTime() - (new Date(user.created_date)).getTime();
+function unfollow(user: Twit.Twitter.User) {
+	const time_diff: number = (new Date()).getTime() - (new Date((<any>user).created_date)).getTime();
 	if (time_diff < MIN_15) return; // Return if friendship was created less than 5 days agos
 
-	t_client.post('friendships/destroy', {user_id: user.id_str},  function(e: any, u: any, raw: any) {
+	T.post('friendships/destroy', <Twit.Params>{ user_id: user.id_str },  function(e: any, u: any, raw: any) {
 		if (e) error(e);
 	});
 }
 
-function unfollowBatch(name: any) {
+function unfollowBatch(name: string) {
 	// log("Starting masse unfollow");
-	t_client.get('friends/list', {screen_name: name, include_user_entities: false, skip_status: true, count: 40},  function(e: any, answer: any, raw: any) {
+	T.get('friends/list', <Twit.Params>{ screen_name: name, include_user_entities: false, skip_status: true, count: 40 },  function(e: any, answer: any, raw: any) {
 		if (e) error(e);
-		else for (let user of answer.users) unfollow(user);
+		else for (let user of <[Twit.Twitter.User]>answer.users) unfollow(user);
 	});
 }
 
-function retweet(tweet: any) {
-	t_client.post('statuses/retweet', {id: tweet.id_str},  function(e: any, t: any, raw: any) {
+function retweet(tweet: Twit.Twitter.Status) {
+	T.post('statuses/retweet', { id: tweet.id_str },  function(e: any, t: any, raw: any) {
 		if (e) error(e);
 	});
 }
 
-function engage(tweet: any) {
+function engage(tweet: Twit.Twitter.Status) {
 	// Prevent useless engagement
-	if (!isTweet(tweet) || // not a tweet
-			tweet.retweeted_status || // is a retweet
+	if (tweet.retweeted_status || // is a retweet
 			tweet.quoted_status ||  // is a quote
 			tweet.retweeted || // allready been retweeted,
-			tweet.user.screen_name.search(/b(o|0)t/i) || // might be a bot spooter. Shit bag
+			tweet.user.screen_name.search(/b(o|0)t/i) != -1 || // might be a bot spooter. Shit bag
 			tweet.user.followers_count < 500 || // need to have a least 500 folowers
-			tweet.user.followers_count*1.5 < tweet.user.friends_count || // need to have 0.5 more followers than subscriptions
-			tweet.text.toLowerCase().includes('steam')) return; // it's for a steam key
+			tweet.user.followers_count < tweet.user.friends_count*1.5 || // need to have 0.5 more followers than subscriptions
+			tweet.text.toLowerCase().search(/steam/i) != -1) return; // it's for a steam key
 
 	setTimeout(()=> {
 		success("Engage tweet " + tweet.id_str + " " + tweet.user.screen_name);
 		retweet(tweet);
 		follow(tweet.user);
-	}, 1000*60*15); // Engage 15 minutes later to act more like a normal person
+	}, 1); // Engage 15 minutes later to act more like a normal person
 }
 
 
