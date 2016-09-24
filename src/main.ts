113d7e11920f7aac7843a768eb34d52fd5011bb7
artonge <artonge.C@gmail.com>
2016-09-24 15:04:50 +0200

    minor optimisations

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import { log } from './log';
 
 
 const SEARCHS: [string] = [
-  "retweet a gagner",
-  "retweet to win",
-  "RT to win",
-  "RT concours"
+  "retweet gagne follow"   , "RT gagne follow"   ,
+  "retweet gagner follow"  , "RT gagner follow"  ,
+  "retweet win follow"     , "RT win follow"     ,
+  "retweet concours follow", "RT concours follow"
 ];
 
 const FOLLOW_HISTORY: Array<number> = [];
@@ -27,9 +27,9 @@ stream.on('tweet', engage);
 stream.on('connected', ()=>log.timeline("connected"));
 stream.on('reconnect', ()=>log.timeline("reconnect"));
 stream.on('disconnect', (disconnect_object: any)=>log.timeline("disconnect" + JSON.stringify(disconnect_object)));
-stream.on('limit'     , (limit_object:      any)=>log.warning( "limit"      + JSON.stringify(limit_object     )));
-stream.on('warning'   , (warning_object:    any)=>log.warning( "warning"    + JSON.stringify(warning_object   )));
-stream.on('blocked'   , (blocked_object:    any)=>log.warning( "blocked"    + JSON.stringify(blocked_object   )));
+stream.on('limit'     , (limit_object:      any)=>log.warning( "limit   "   + JSON.stringify(limit_object     )));
+stream.on('warning'   , (warning_object:    any)=>log.warning( "warning "   + JSON.stringify(warning_object   )));
+stream.on('blocked'   , (blocked_object:    any)=>log.warning( "blocked "   + JSON.stringify(blocked_object   )));
 
 
 function canFollow(): boolean {
@@ -44,8 +44,7 @@ function canFollow(): boolean {
 }
 
 function follow(user: Twit.Twitter.User) {
-	if (user.following) { log.error("Allready following");   return; }
-	if (!canFollow())   { /*error("Follow limit reached");*/ return; }
+	if (user.following || !canFollow()) return;
 
 	T.post('friendships/create', <Twit.Params>{ user_id: user.id_str },  function(e: any, u: any, raw: any) {
 		if (e) log.error(e);
@@ -55,7 +54,7 @@ function follow(user: Twit.Twitter.User) {
 
 function unfollow(user: Twit.Twitter.User) {
 	const time_diff: number = (new Date()).getTime() - (new Date((<any>user).created_date)).getTime();
-	if (time_diff < MIN_15) return; // Return if friendship was created less than 5 days agos
+	if (time_diff < 1000*60*60*24*100) return; // Return if friendship was created less than 100 days agos
 
 	T.post('friendships/destroy', <Twit.Params>{ user_id: user.id_str },  function(e: any, u: any, raw: any) {
 		if (e) log.error(e);
@@ -80,17 +79,19 @@ function engage(tweet: Twit.Twitter.Status) {
 	// Prevent useless engagement
 	if (tweet.retweeted_status || // is a retweet
 			tweet.quoted_status ||  // is a quote
-			tweet.retweeted || // allready been retweeted,
+			tweet.retweeted || // allready been retweeted
+			tweet.in_reply_to_status_id || // is a reply
 			tweet.user.screen_name.search(/b(o|0)t/i) != -1 || // might be a bot spooter. Shit bag
-			tweet.user.followers_count < 500 || // need to have a least 500 folowers
-			tweet.user.followers_count < tweet.user.friends_count*1.5 || // need to have 0.5 more followers than subscriptions
-			tweet.text.toLowerCase().search(/steam/i) != -1) return; // it's for a steam key
+			tweet.text.search(/bet/i) != -1 || // probably an ad for betting site
+			tweet.user.followers_count < 300 || // need to have a least 500 folowers
+			tweet.user.followers_count < tweet.user.friends_count*1.3 || // need to have 0.3 more followers than subscriptions
+			tweet.text.search(/steam/i) != -1) return; // it's for a steam key
 
+	log.engagement("Engage tweet " + tweet.id_str + " " + tweet.user.screen_name);
 	setTimeout(()=> {
-		log.engagement("Engage tweet " + tweet.id_str + " " + tweet.user.screen_name);
 		retweet(tweet);
 		follow(tweet.user);
-	}, MIN_15); // Engage 15 minutes later to act more like a normal person
+	}, 0); // Engage 15 minutes later to act more like a normal person
 }
 
 
