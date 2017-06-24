a3a9cbffa0a565ef84e1c114c9985b28cbe3a668
Louis Chemineau <artonge.c@gmail.com>
2017-06-24 20:52:18 +0300

    Add credentials file

---
+++ src/credentials.ts
@@ -0,0 +1,6 @@
+export const credentials = {
+  consumer_key        : process.env.consumer_key,
+  consumer_secret     : process.env.consumer_secret,
+  access_token        : process.env.access_token,
+  access_token_secret : process.env.access_token_secret
+}
